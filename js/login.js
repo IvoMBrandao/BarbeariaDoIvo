ee077fb625f2db29f0f605b15a836409f807929a
IvoMbrandao <113533224+IvoMBrandao@users.noreply.github.com>
2023-09-05 22:44:36 -0300

    validacao corrigida

--- js/login.js
+++ js/login.js
@@ -15,16 +15,35 @@ buttonLogin.addEventListener('click', () => {
 
    autenticar(userEmail,userSenha)
 
- window.open('usuario.html', '_selfe')
    
 });
 
-function autenticar(email,senha){
- const url=`http://localhost:3400`;
- fetch(`${url}/login`,{
-  method:'POST',
-  body: JSON.stringify({email,senha})
- }).then(response => response.json())
- .then(response => console.log(response))
- .catch(erro = console.log(erro))
+function autenticar(email, senha){
+   const urlBase = `http://localhost:3400`;
+
+   fetch(`${urlBase}/login`, {
+    method:'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, senha})
+   })
+   .then(response => response = response.json())
+   .then(response => {
+
+       if(!!response.mensagem){
+        alert(response.mensagem);
+        return;
+
+       }else{
+        
+        
+       
+
+      
+            window.open('usuario.html', '_self');
+    
+        
+       }
+    });
 }
